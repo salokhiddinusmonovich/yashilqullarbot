cef74b9ecbe2361e03f6ea8f35aa1dd39030ff7a
Salokhiddin <usmonovsalokhiddin@gmail.com>
2026-03-23 22:52:58 +0500

    s

--- dj_ac/urls.py
+++ dj_ac/urls.py
@@ -1,27 +1,16 @@
-"""
-URL configuration for dj_ac project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse # Добавь этот импорт
+
+# Создадим быструю функцию для главной страницы
+def home_view(request):
+    return HttpResponse("<h1>Salokhiddin, Django is working!</h1><p>Go to <a href='/admin/'>/admin/</a> to login.</p>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view), # Добавь этот путь для главной страницы
 ]
-
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
