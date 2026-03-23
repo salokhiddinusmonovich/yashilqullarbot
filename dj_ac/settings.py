989a65e0bc06f028ef5ec8bb87dde87072ec4dec
Salokhiddin <usmonovsalokhiddin@gmail.com>
2026-03-23 22:31:56 +0500

    s

--- dj_ac/settings.py
+++ dj_ac/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG"))
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(',')
+ALLOWED_HOSTS = ['173.249.19.32', 'localhost', '127.0.0.1']
 
 
 # Application definition
