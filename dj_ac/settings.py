4ecc700de23e7561eb10bd2a9b1d2c3ccb2057b4
Salokhiddin <usmonovsalokhiddin@gmail.com>
2026-03-26 17:52:31 +0500

    salom

--- dj_ac/settings.py
+++ dj_ac/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': POSTGRES_DB,
         'USER': POSTGRES_USER,
-        'PASSWORD': POSTGRES_PASS,
+        'PASSWORD': 'postgres',
         'HOST': POSTGRES_HOST,
         'PORT': POSTGRES_PORT,
         'ATOMIC_REQUESTS': True,
