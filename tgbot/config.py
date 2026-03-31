09af60c663feba7bde678136de2c9ecc5fec324d
Salokhiddin <usmonovsalokhiddin@gmail.com>
2026-03-31 22:23:22 +0500

    salom

--- tgbot/config.py
+++ tgbot/config.py
@@ -1,17 +1,5 @@
 from dataclasses import dataclass
 
-from environs import Env
-
-
-# @dataclass
-# class DbConfig:
-#     host: str
-#     password: str
-#     user: str
-#     name: str
-#     port: str
-
-
 @dataclass
 class Redis:
     use_redis: bool
@@ -19,47 +7,33 @@ class Redis:
     port: str
     password: str | None
 
-
 @dataclass
 class TgBot:
     token: str
     admin_ids: list[int]
 
-
 @dataclass
 class Miscellaneous:
     other_params: str = None
-
 
 @dataclass
 class Config:
     tg_bot: TgBot
     redis: Redis
-    # db: DbConfig
     misc: Miscellaneous
 
-
 def load_config(path: str = None):
-    env = Env()
-    env.read_env(path)
-
+    # МЫ ВООБЩЕ УБРАЛИ ENVIRONS И ПИШЕМ ДАННЫЕ ПРЯМО ТУТ
     return Config(
         tg_bot=TgBot(
-            token=env.str("BOT_TOKEN"),
-            admin_ids=list(map(int, env.list("ADMINS"))),
+            token="8597081931:AAHrLlthINCN8nIZp_zh3WEbzfc-5GhoHmw",
+            admin_ids=[111],
         ),
         redis=Redis(
-            host=env.str("REDIS_HOST"),
-            port=env.str("REDIS_PORT"),
-            password=env.str("REDIS_PASSWORD"),
-            use_redis=env.bool("USE_REDIS", 'False')
+            host="redis",  # Название сервиса из docker-compose
+            port="6379",
+            password=None,
+            use_redis=True
         ),
-        # db=DbConfig(
-        #     host=env.str('POSTGRES_HOST'),
-        #     password=env.str('POSTGRES_PASSWORD'),
-        #     user=env.str('POSTGRES_USER'),
-        #     name=env.str('POSTGRES_DB'),
-        #     port=env.str('POSTGRES_PORT')
-        # ),
         misc=Miscellaneous()
     )
