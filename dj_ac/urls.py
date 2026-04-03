20d418121b4f7fc80badebc55b8641d83025617f
Salokhiddin <usmonovsalokhiddin@gmail.com>
2026-04-04 01:20:10 +0500

    salom

--- dj_ac/urls.py
+++ dj_ac/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse # Добавь этот импорт
+from django.urls import re_path
+from django.views.static import serve
 
 # Создадим быструю функцию для главной страницы
 def home_view(request):
@@ -12,5 +14,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view), # Добавь этот путь для главной страницы
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Добавляем это принудительно для медиа:
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
