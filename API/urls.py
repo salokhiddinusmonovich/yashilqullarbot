8bca98bf2ff041d52bd1f1529b9f4755c082ac75
Salokhiddin <usmonovsalokhiddin@gmail.com>
2026-06-27 16:44:34 +0500

    salom

--- API/urls.py
+++ API/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .api import RegisterView, LoginView, LogoutView
+from .api import TelegramLoginView, LogoutView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', TelegramLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
